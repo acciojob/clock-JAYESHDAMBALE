a3627c651bf7ac32978e82cae71e51ba8d707554
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-05 21:00:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+
+let now = new Date();
+
+// let currentDateTime = now.toLocalString();
+// console.log(${currentDateTime});
+
+        const currentDateTime = now.toLocaleString();
+        console.log(`Current Date and Time: ${currentDateTime}`);
+        // Example output: "11/5/2025, 8:57:00 PM" (format varies by locale)
