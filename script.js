587c075e1825220d13476f1704bdc5689c2ab6d4
Jayesh Dambale <88830136+JAYESHDAMBALE@users.noreply.github.com>
2025-11-05 21:17:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,9 @@
-//your JS code here. If required.
+function updateTimer() {
+	let now = new Date().toLocaleString();
+	document.getElementById("timer").textContent = now;
+}
 
-let now = new Date();
+setInterval(updateTimer,1000);
 
-let currentDateTime = now.toLocaleString();
-document.getElementById("timer").textContent = currentDateTime
-
-console.log(`${currentDateTime}`);
-
-// Function to update timer every second
-// function updateTimer() {
-//   const now = new Date();
-//   const currentDateTime = now.toLocaleString(); // correct method
-//   document.getElementById("timer").textContent = currentDateTime;
-// }
-
-// // call function every 1 second
-// setInterval(updateTimer, 1000);
-
-// // call immediately when page loads
-// updateTimer();
-
-
-
+updateTimer();
 
